cb20deab851de5f4f059fd1b99163bfa78b961a5
RatRyan <ryantratajczak@gmail.com>
2024-02-14 14:25:08 -0700

    test push

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+	fish_logo
+end
+
+# Constants
+set -gx EDITOR nvim
+
+# Init bruv bo'oh'o'wa'er
+starship init fish | source
+zoxide init fish | source
+
+# Binaries
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
