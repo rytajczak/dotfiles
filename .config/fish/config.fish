10f85b309e7f3a006bd45638de4942bdc063b3e7
RatRyan <ryantratajczak@gmail.com>
2024-02-17 16:09:08 -0700

    update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,34 @@ if status is-interactive
 end
 
 function fish_greeting
-	fish_logo
+    set o (set_color 'red')
+    set m (set_color 'f70')
+    set i (set_color 'yellow')
+
+    set user (whoami)
+    set host (prompt_hostname)
+
+    echo '                 '$o'___
+  ___======____='$m'-'$i'-'$m'-='$o')
+/T            \_'$i'--='$m'=='$o')    '$user'@'$host'
+'\[' \ '$m'('$i'O'$m')   '$o'\~    \_'$i'-='$m'='$o')    Fish fear me
+ \      / )J'$m'~~    '$o'\\'$i'-='$o')    Men turn their eyes away from me
+  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)
+   \_____/JJJ'$m'~~'$i'~~    '$o'\\
+   '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\
+  (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_
+  '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__
+   '$o'\V    '$m'\\\\'$o'\) =='$m'=_____   '$i'\\\\\\\\'$m'\\\\
+          '$o'\V)     \_) '$m'\\\\'$i'\\\\JJ\\'$m'J\)
+                      '$o'/'$m'J'$i'\\'$m'J'$o'T\\'$m'JJJ'$o'J)
+                      (J'$m'JJ'$o'| \UUU)
+                       (UU)'(set_color normal)
 end
 
 # Alias
 alias ls="exa -l"
+alias ga="git add ."
+alias gp="git push"
 alias gg="lazygit"
 
 # Constants
