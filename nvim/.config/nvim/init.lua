229aa53b9c9562617c58483e70d84a1fd093446a
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-06-25 13:59:37 -0600

    update

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+
+require 'config.options'
+require 'config.keymaps'
+require 'config.lazy'
+
+-- vim: t=2 sts=2 sw=2 et
