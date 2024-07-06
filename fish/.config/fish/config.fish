03279efaa84d0f78a9df8ffbb00f5c3fc6e4789e
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-07-06 13:27:08 -0600

    update

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+# General configuration
+set -gx PATH /usr/local/bin $PATH
+set -gx LANG en_US.UTF-8
+set -U fish_greeting
+
+# Interactive configruation
+if status is-interactive
+  set -gx EDITOR nvim
+
+  alias fnix="nix-shell --run fish"
+
+  starship init fish | source
+  zoxide init --cmd cd fish | source
+end
+
