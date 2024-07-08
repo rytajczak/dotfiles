14dc1a4793beae6f822d72a2ce0d7c1b0879d22e
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-07-07 22:27:10 -0600

    add wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,9 +4,10 @@ local config = wezterm.config_builder()
 config.default_prog = { "tmux" }
 
 config.font_size = 14
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("Comic Mono")
 
-config.color_scheme = "Dark+"
+config.color_scheme = "Monokai (base16)"
+config.colors = { background = "#161414" }
 
 config.enable_tab_bar = false
 config.enable_wayland = false
