4bb360ba387fc9c662e18e048a35b698a7d3b38d
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-07-12 10:47:25 -0600

    update nvim

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,3 +1,9 @@
 return {
-  { 'echasnovski/mini.nvim', version = false },
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      require('mini.pairs').setup()
+      require('mini.comment').setup()
+    end
+  },
 }
