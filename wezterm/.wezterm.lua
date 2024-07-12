e805e2419b0e12b2e47d4478991e052dd90ceb97
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-07-11 23:41:08 -0600

    update nvim

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,10 +4,10 @@ local config = wezterm.config_builder()
 config.default_prog = { "tmux" }
 
 config.font_size = 14
--- config.font = wezterm.font("Comic Mono")
+config.font = wezterm.font("0xProto Nerd Font Mono")
 
 config.color_scheme = "Monokai (base16)"
-config.colors = { background = "#161414" }
+-- config.colors = { background = "#161414" }
 
 config.enable_tab_bar = false
 config.enable_wayland = false
