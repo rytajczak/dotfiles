1308ddd15a2c6067cd02ec9e507299d7cd18197a
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-07-14 12:01:22 -0600

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,10 +3,10 @@ local config = wezterm.config_builder()
 
 config.default_prog = { "tmux" }
 
-config.font_size = 14
+config.font_size = 16
 config.font = wezterm.font("0xProto Nerd Font Mono")
 
-config.color_scheme = "Apple System Colors"
+config.color_scheme = "Catppuccin Mocha"
 config.colors = { background = "#161414" }
 
 config.enable_tab_bar = false
