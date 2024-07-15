af1df4c0c310f6db5ecb6b4f02edfb4a4d8708f8
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-07-15 08:56:39 -0600

    update nvim

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { "prettier" },
+			html = { "prettier" },
+			vue = { "prettier" },
+			yaml = { "prettier" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,28 +1,29 @@
 return {
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {'neovim/nvim-lspconfig'},
-  {'L3MON4D3/LuaSnip'},
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    init = function()
-      local lsp_zero = require('lsp-zero')
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "L3MON4D3/LuaSnip" },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		config = function()
+			local lsp_zero = require("lsp-zero")
 
-      lsp_zero.extend_lspconfig()
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
+			-- One day ill know what this does...
+			lsp_zero.extend_lspconfig()
+			lsp_zero.on_attach(function(client, bufnr)
+				lsp_zero.default_keymaps({ buffer = bufnr })
+			end)
 
-      require('mason').setup({})
-      require('mason-lspconfig').setup({
-        ensure_installed = {},
-        handlers = {
-          function(server_name)
-            require('lspconfig')[server_name].setup({})
-          end,
-        },
-      })
-    end
-  },
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+				},
+			})
+		end,
+	},
 }
