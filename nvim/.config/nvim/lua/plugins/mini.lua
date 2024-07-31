1014e79251a343c8399a4065a864b3594de052cf
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-07-30 21:32:08 -0600

    update

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -4,9 +4,6 @@ return {
 		config = function()
 			require("mini.pairs").setup()
 			require("mini.comment").setup()
-			require("mini.completion").setup({
-				delay = { completion = 20, info = 100, signature = 50 },
-			})
 		end,
 	},
 }
