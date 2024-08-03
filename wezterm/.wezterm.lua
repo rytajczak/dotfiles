ce583b0c1cae8ccd9f0e89b27f56b8b5e22b9758
Ryan Ratajczak <ryantratajczak@gmail.com>
2024-08-03 02:49:11 -0600

    update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,13 +1,13 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.default_prog = { "tmux" }
+config.default_prog = { "fish" }
 
 config.font_size = 14
-config.font = wezterm.font("0xProto Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 
 config.color_scheme = "carbonfox"
-config.colors = { background = "#242424" }
+config.colors = { background = "#1a1a1a" }
 
 config.enable_tab_bar = false
 config.enable_wayland = false
