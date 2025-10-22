d9c0cfceb64cc244f43de43c240d4a42e429091e
Ryan Ratajczak <rytajczak@gmail.com>
2025-10-22 14:12:41 -0600

    Add keybinds descriptions

--- .config/nvim/plugin/overseer.lua
+++ .config/nvim/plugin/overseer.lua
@@ -2,5 +2,5 @@ MiniDeps.add({ source = "stevearc/overseer.nvim", checkout = "v1.6.0" })
 
 require("overseer").setup()
 
-vim.keymap.set("n", "<leader>r", ":OverseerRun<CR>")
-vim.keymap.set("n", "<leader>o", ":OverseerToggle<CR>")
+vim.keymap.set("n", "<leader>r", ":OverseerRun<CR>", { desc = "Run task" })
+vim.keymap.set("n", "<leader>o", ":OverseerToggle<CR>", { desc = "Toggle overseer" })
